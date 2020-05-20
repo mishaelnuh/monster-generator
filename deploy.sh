024bebaf4a068bdaada1d15c71229c5385051f37
Mishael Nuh <mishael.ebel.nuh@gmail.com>
2020-05-20 17:40:10 -0400

    Add the deploy script

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+echo 'dish.mnuh.org' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:mishaelnuh/mish-dish.git master:gh-pages
+
+cd -
