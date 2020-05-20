6a0e3a027fa831abc199007303ebf30653c7d4bc
Mishael Nuh <mishael.ebel.nuh@gmail.com>
2020-05-20 17:44:18 -0400

    Rename to dish.

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:mishaelnuh/mish-dish.git master:gh-pages
+git push -f git@github.com:mishaelnuh/dish.git master:gh-pages
 
 cd -
