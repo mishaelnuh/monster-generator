efc77859d170475321cf52c30bccce25a0ac94da
Mishael Nuh <mishael.ebel.nuh@gmail.com>
2020-06-27 00:01:17 -0400

    Change repo for another thing

--- deploy.sh
+++ deploy.sh
@@ -10,12 +10,12 @@ npm run build
 cd dist
 
 # if you are deploying to a custom domain
-echo 'dish.mnuh.org' > CNAME
+echo 'monster.mnuh.org' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:mishaelnuh/dish.git master:gh-pages
+git push -f git@github.com:mishaelnuh/monster-generator.git master:gh-pages
 
 cd -
